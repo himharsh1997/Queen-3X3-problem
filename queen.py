d52c3c3df722c24025013c2998799a22439d46a9
Himanshu Joshi <himharsh1997@gmail.com>
2019-12-25 00:25:36 +0530

    Now optimised path is founded for finding queen.

--- queen.py
+++ queen.py
@@ -1,20 +1,33 @@
-arr = [['+', '+', 'p'],['+', 'm', '+'],['+', '+', '+']]
+#!/usr/bin/python
+import math 
 
-def move(n, i, j, arr, path):
+possiblePaths = []
+def displayPathtoPrincess(n, i, j, arr, path):
   if(arr[i][j] == 'p'):
-    return path
-  if(i < n-1 and arr[i+1][j] != '-'):
-    arr[i][j] = '-'
-    return move(n, i+1, j, arr, path + ' DOWN')          
-  if(i > 0 and arr[i-1][j] != '-'):
-    arr[i][j] = '-'
-    return move(n, i-1, j, arr, path + ' \nUP')
-  if(j > 0 and arr[i][j-1] != '-'):
-    arr[i][j] = '-'
-    return move(n, i, j-1, arr, path + ' \nLEFT')
-  if(j < n-1 and arr[i][j+1] != '-'):  
-    arr[i][j] = '-'
-    return move(n, i, j+1, arr, path  + ' \nRIGHT')
-  return ''
+    possiblePaths.append(path)
+  if(i < n-1 and arr[i+1][j] != '+'):
+    arr[i][j] = '+'
+    displayPathtoPrincess(n, i+1, j, arr, path + ['DOWN'])          
+  if(i > 0 and arr[i-1][j] != '+'):
+    arr[i][j] = '+'
+    displayPathtoPrincess(n, i-1, j, arr, path + ['UP'])
+  if(j > 0 and arr[i][j-1] != '+'):
+    arr[i][j] = '+'
+    displayPathtoPrincess(n, i, j-1, arr, path + ['LEFT'])
+  if(j < n-1 and arr[i][j+1] != '+'):  
+    arr[i][j] = '+'
+    displayPathtoPrincess(n, i, j+1, arr, path + ['RIGHT'])
+  return True
 
-print(move(3, 1, 1, arr, ''))
+m = int(input())
+grid = [] 
+for i in range(0, m): 
+    grid.append(list(input().strip()))
+
+displayPathtoPrincess(m, math.floor(m/2), math.floor(m/2) , grid, list([]))
+
+bestPath = []
+for path in possiblePaths:
+  if(len(path)> len(bestPath)):
+    bestPath = path
+print('\n'.join(bestPath))  
